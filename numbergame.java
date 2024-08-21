559864e0e60826a6a8b1ca6705a9078ec9cb98e3
Ritesh Sahoo <162371198+Sweet-Billi@users.noreply.github.com>
2024-08-21 21:44:49 +0530

    Update numbergame.java

--- numbergame.java
+++ numbergame.java
@@ -1,119 +1,87 @@
 package internship;
-import java.util.Random;
-	import java.util.Scanner;
-public class numbergame {
-		public static void main(String[]args) {
-			Scanner sc = new Scanner (System.in);
-			
-			//initialization of values 
-			
-			int userguess,computerguess,totalattempts,totalmatches,win;
-			
-			totalattempts = 0;
-			totalmatches = 1;
-			win = 0;
-			int max = 100;
-			String userreply= "N";
-			boolean answer = false;
-			
-		//Generate a random number within a specified range, such as 1 to 100.
-			computerguess = (int) (1+Math.random()*(max-1));
-			
-				//limit the number of attempts the user has to guess the number.
-			
-				int maxattempts=0;
-				int attempts = 0;
-			System.out.println("Can you guess the number in 10 attempts"); 
-	    	 
-	    	 /*Compare the user's guess with the generated number and provide feedback on whether the guess
-	    	 is correct, too high, or too low.*/
-	    	 
-			while(!answer) {
-				maxattempts++;
-				
-				//Prompt the user to enter their guess for the generated number.
-				
-				System.out.println("Enter your guessed number betweeen 1 to 100");
-				userguess = sc.nextInt();
-				attempts++;
-				if(userguess<computerguess) {
-					System.out.println("too low, Try again");
-				}
-				else if(userguess>computerguess) {
-					System.out.println("too high , Try again ");
-			}
-				else if(userguess==computerguess){
-					System.out.println("Congratulation ,You guessed it correct");
-					win++;
-					answer = true;
-					System.out.println("Attempts taken to guess correctly are :"+attempts);
-					break;
-				}
-					else {
-					 System.out.println("Could not guess collectly ");
-				     System.out.println("Dont loose so easily , You can do it !!!");
-				     System.out.println();
-					}
-					System.out.println();
-					totalattempts+=attempts;
-					if(maxattempts==10) {
-						System.out.println("You have reached to the maximum try , please Restart");
-						break;
-					}
-				}
-			System.out.println("Wanna play another round ??");
-		     System.out.println();
-		     System.out.println("For YES type 'Y' , For NO type 'N'");
-		      userreply=sc.next();
-		      
-		if(userreply == "Y"||userreply=="y") {
-			totalmatches++;
-           while(!answer) {
-				maxattempts++;
-				//Prompt the user to enter their guess for the generated number.
-				
-				System.out.println("Enter your guessed number betweeen 1 to 100");
-				userguess = sc.nextInt();
-				attempts++;
-				if(userguess<computerguess) {
-					System.out.println("too low, Try again");
-				}
-				else if(userguess>computerguess) {
-					System.out.println("too high , Try again ");
-			}
-				else if(userguess==computerguess){
-					System.out.println("Congratulation ,You guessed it correct");
-					win++;
-					answer = true;
-					System.out.println("Attempts taken to guess correctly are :"+attempts);
-					break;
-				}
-					else {
-					 System.out.println("Could not guess collectly ");
-				     System.out.println("Dont loose so easily , You can do it !!!");
-				     System.out.println();
-					}
-					System.out.println();
-					totalattempts+=attempts;
-					if(maxattempts==10) {
-						System.out.println("You have reached to the maximum try , please Restart");
-						break;
-					}
-				}
-		}	
-		
-		//score display
-		
-		else {
-			
-			
-			System.out.println("Here is your Score");
-			for(int i =0;i<50;i++) {
+
+import java.util.Scanner;
+
+public class numgame {
+
+	public static void main(String[] args) {
+		int userguess,computerguess;
+		int totalmatches=0;
+        int noofwins = 0;
+        int totalattempts = 0;
+        int maxAttempts = 10;
+        Scanner sc = new Scanner(System.in);
+        
+		 while (true) {
+			 totalmatches++;
+			 computerguess = (int) (1+Math.random()*(100-1));
+	            int attempts = 0;
+	            boolean Win = false;
+	            System.out.println("Guess a number between 1 and 100:");
+	            
+	            while (attempts < maxAttempts) {  	
+	                int userGuess = sc.nextInt();
+	                attempts++;
+	                totalattempts++;
+
+	                if (userGuess < computerguess) {
+	                    System.out.println("Too low!,Try again ");
+	                    System.out.println();
+	                } else if (userGuess > computerguess) {
+	                    System.out.println("Too high!, Try again");
+	                    System.out.println();
+	                } else {
+	                    System.out.println("Congratulation ,You guessed it correct");
+	                    noofwins++;
+	                    Win = true;
+	                    break;
+	                }
+	            }
+	            if (!Win) {
+						 System.out.println("You have reached to the maximum try , please Restart");
+						 System.out.println("The number was: " + computerguess);
+						 System.out.println();
+					     System.out.println("Dont loose so easily , You can do it !!!");
+	            }
+	            System.out.println("Wanna play another round ??");
+			     System.out.println();
+	            System.out.println("Do you want to play again? for 'YES' press 1 and for 'NO' press 2 ");
+	            int userreply= sc.nextInt();
+	            if(userreply==1) {         
+		            while (attempts < maxAttempts) {  	
+		                int userGuess = sc.nextInt();
+		                attempts++;
+		                totalattempts++;
+
+		                if (userGuess < computerguess) {
+		                    System.out.println("Too low!,Try again ");
+		                    System.out.println();
+		                } else if (userGuess > computerguess) {
+		                    System.out.println("Too high!, Try again");
+		                    System.out.println();
+		                } else {
+		                    System.out.println("Congratulation ,You guessed it correct");
+		                    noofwins++;
+		                    Win = true;
+		                    break;
+		                }
+		            }
+	            }
+	            else if (userreply==2){
+	                break;
+	            }
+	            else {
+	            	System.out.println("Please enter a valid Option");
+	            	break;
+	            }
+	        } 
+		 System.out.println();
+		 for(int i =0;i<50;i++) {
 				System.out.print("=");
 			}
-			System.out.println();
+		 System.out.println();
 			System.out.println("Total Attempts"+"  |  "+"Total Matches"+"  |  "+"Wins");
-		    System.out.println(" "+totalattempts+"             |   "+totalmatches+"             |  "+win+" ");
+		    System.out.println(" "+totalattempts+"             |   "+totalmatches+"             |  "+noofwins+" ");
 			System.out.println();
 			for(int i =0;i<50;i++) {
 				System.out.print("=");
@@ -121,6 +89,5 @@ public class numbergame {
 			System.out.println();
 				System.out.println("End of the game :)Hope you enjoyed ");
 	}
-	}
-}
 
+	}
